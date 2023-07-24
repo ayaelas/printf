b96f7b854e82fec51827935f6bfac2ba36f792ca
Ductive99 <sou.ductive@gmail.com>
2023-07-24 12:19:33 +0100

    quick fix - removed ')'

--- _printf.c
+++ _printf.c
@@ -18,7 +18,7 @@ int _printf(const char *format,...)
     
     if (!format || (format[0] == '%' && !format[1]))
         return (-1);
-    if (format[0] == '%' && format[0] == ' ' && !format[2]))
+    if (format[0] == '%' && format[0] == ' ' && !format[2])
 		return (-1);
 	
 
