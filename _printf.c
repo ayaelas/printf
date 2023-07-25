e75ad90a932476d5f2fd8cefcf1c92da94b4b775
Ductive99 <sou.ductive@gmail.com>
2023-07-25 14:24:17 +0100

    undo

--- _printf.c
+++ _printf.c
@@ -18,7 +18,7 @@ int _printf(const char *format, ...)
 
 	va_start(ap, format);
 	if (!format || (format[0] == '%' && !format[1]))
-		return (0);
+		return (-1);
 	if (format[0] == '%' && format[1] == ' ' && !format[2])
 		return (-1);
 
